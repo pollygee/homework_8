5ab7ffd9eceb18530f7a4593510e66aca918bfe6
Polly Sileo <pollysileo@gmail.com>
2015-06-05 16:22:42 -0400

    Able to mark tasks as done

--- model-skeleton/todo.rb
+++ model-skeleton/todo.rb
@@ -4,7 +4,6 @@ require './lib/all'
 require 'date'
 
 puts "Welcome to Todo"
-puts "Command options \nadd [list_name] [todo_item]"
 class ToDo
   def add_item new_list_name, new_todo_item
     list = List.where(list_name: new_list_name).first_or_create!
@@ -15,8 +14,14 @@ class ToDo
     date = Date.parse date
     p = Item.where(todo: item).first
     p.due_date = date
+    p.save
   end
 
+  def add_done item
+    p = Item.where(todo: item).first
+    p.done = true
+    p.save
+  end
 end
 
 todo  = ToDo.new
@@ -28,6 +33,10 @@ if command == "add"
 elsif command == 'due'
   item, date = ARGV
   todo.add_due_date item, date
+elsif command == 'done'
+  item = ARGV.first
+  todo.add_done item
 else
-  puts "Command I know are \n add [list_name] [todo_item]"
+  puts "Command I know are \n add [list_name] [todo_item]\n
+            due [item] [time - year, month, day]\n"
 end
